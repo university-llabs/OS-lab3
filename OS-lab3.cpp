a4d6c76e3823fff7fdb84a31ebd673e85c6d1d5a
aleskitta <alesyadamasevich@gmail.com>
2025-10-29 00:43:48 +0300

    -Implement thread termination and continuation

--- OS-lab3.cpp
+++ OS-lab3.cpp
@@ -25,7 +25,7 @@ int main() {
     for (int i = 0; i < array_size; i++) {
         cout << arr[i] << " ";
     }
-    cout << std::endl;
+    cout << endl;
 
     int num_threads;
     cout << "Enter the number of marker threads: ";
@@ -86,6 +86,67 @@ int main() {
     cout << "Launching all streams..." << endl;
     SetEvent(start_event);
 
+    vector<bool> active_threads(num_threads, true);
+    int active_count = num_threads;
+
+    while (active_count > 0) {
+        vector<HANDLE> current_stop_events;
+        for (int i = 0; i < num_threads; i++) {
+            if (active_threads[i]) {
+                current_stop_events.push_back(stop_events[i]);
+            }
+        }
+
+        WaitForMultipleObjects(
+            current_stop_events.size(),
+            current_stop_events.data(),
+            TRUE,           // ∆дЄм все событи€
+            INFINITE
+        );
+
+        cout << "\nCurrent state of the array: ";
+        for (int i = 0; i < array_size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+
+        int thread_to_terminate;
+        cout << "Enter the stream number to complete: ";
+        cin >> thread_to_terminate;
+
+        if (thread_to_terminate < 1 || thread_to_terminate > num_threads ||
+            !active_threads[thread_to_terminate - 1]) {
+            cout << "Error: Invalid stream number!" << endl;
+            continue;
+        }
+        SetEvent(terminate_events[thread_to_terminate - 1]);
+
+        WaitForSingleObject(threads[thread_to_terminate - 1], INFINITE);
+
+        cout << "The state of the array after the thread has completed "
+            << thread_to_terminate << ": ";
+        for (int i = 0; i < array_size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        for (int i = 0; i < num_threads; i++) {
+            if (active_threads[i] && i != thread_to_terminate - 1) {
+                SetEvent(continue_events[i]);
+            }
+        }
+
+        active_threads[thread_to_terminate - 1] = false;
+        active_count--;
+ 
+        for (int i = 0; i < num_threads; i++) {
+            if (active_threads[i]) {
+                ResetEvent(stop_events[i]);
+            }
+        }
+    }
+    
    return 0;
 }
 DWORD WINAPI marker_thread(LPVOID param) {
@@ -115,9 +176,8 @@ DWORD WINAPI marker_thread(LPVOID param) {
             break;
         }
     }
-
-    cout << "Thread " << data->thread_id << " stopped. "
-        << "Number of marks:: " << marks_count << ", "
+    cout << "Thread " << data->thread_id << " stopped. " 
+        << "Number of marks: " << marks_count << ", "
         << "Lock index: " << last_index << endl;
 
     SetEvent(data->stop_event);
